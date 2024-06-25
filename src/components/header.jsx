8ed6749ab897f15574f995ae14d464c44e721eb9
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-06-25 12:53:58 +0530

    added mobile nav

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,29 +5,10 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 import { Button } from './ui/button'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 
-export default function Header() {
-    return (
-        <header className="py-4 text-white xl:py-12">
-            <div className="container mx-auto flex items-center justify-between">
-                <Link href="/">
-                    <h1 className="text-4xl font-semibold">
-                        Shubham
-                        <span className="text-accent">.</span>
-                    </h1>
-                </Link>
-                <div className="hidden items-center gap-4 xl:flex">
-                    <Nav />
-                    <Link href="/contact">
-                        <Button>Hire Me!</Button>
-                    </Link>
-                </div>
-
-                <div className="xl:hidden">mobile-view</div>
-            </div>
-        </header>
-    )
-}
+import { CiMenuFries } from 'react-icons/ci'
+import Image from 'next/image'
 
 const links = [
     {
@@ -52,6 +33,31 @@ const links = [
     },
 ]
 
+export default function Header() {
+    return (
+        <header className="py-4 text-white xl:py-12">
+            <div className="container mx-auto flex items-center justify-between">
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Shubham
+                        <span className="text-accent">.</span>
+                    </h1>
+                </Link>
+                <div className="hidden items-center gap-8 xl:flex">
+                    <Nav />
+                    <Link href="/contact">
+                        <Button>Hire Me!</Button>
+                    </Link>
+                </div>
+
+                <div className="xl:hidden">
+                    <MobileNav />
+                </div>
+            </div>
+        </header>
+    )
+}
+
 const Nav = () => {
     const pathname = usePathname()
 
@@ -67,5 +73,38 @@ const Nav = () => {
                 </Link>
             ))}
         </nav>
+    )
+}
+
+const MobileNav = () => {
+    const pathname = usePathname()
+    return (
+        <Sheet>
+            <SheetTrigger className="flex items-center justify-center">
+                <CiMenuFries className="text-accent" />
+            </SheetTrigger>
+            <SheetContent className="flex flex-col">
+                <div className="mb-40 mt-32 text-center text-2xl">
+                    <Link href="/">
+                        <h1 className="text-4xl font-semibold">
+                            Shubham
+                            <span className="text-accent">.</span>
+                        </h1>
+                        {/* <Image /> */}
+                    </Link>
+                </div>
+                <nav className="flex flex-col items-center justify-center gap-8">
+                    {links.map((link, i) => (
+                        <Link
+                            key={link.name}
+                            href={link.path}
+                            className={` ${link.path === pathname && 'border-b-2 border-accent text-accent'} text-xl capitalize transition-all hover:text-accent`}
+                        >
+                            {link.name}
+                        </Link>
+                    ))}
+                </nav>
+            </SheetContent>
+        </Sheet>
     )
 }
