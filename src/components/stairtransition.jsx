86650005dcadfe57935951f9c011672716914366
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-06-25 12:22:50 +0530

    added more animation to stairs

--- src/components/stairtransition.jsx
+++ src/components/stairtransition.jsx
@@ -13,6 +13,20 @@ export default function StairTransition() {
                     <div className="pointer-events-none fixed left-0 right-0 top-0 z-40 flex h-screen w-screen">
                         <Stairs />
                     </div>
+                    <motion.div
+                        className="pointer-events-none fixed top-0 h-screen w-screen bg-primary"
+                        initial={{
+                            opacity: 1,
+                        }}
+                        animate={{
+                            opacity: 0,
+                            transition: {
+                                delay: 1,
+                                duration: 0.4,
+                                ease: 'easeInOut',
+                            },
+                        }}
+                    />
                 </div>
             </AnimatePresence>
         </>
@@ -41,7 +55,7 @@ const Stairs = () => {
                 animate="animate"
                 exit="exit"
                 transition={{
-                    duration: 0.3,
+                    duration: 0.15,
                     ease: 'easeInOut',
                     delay: reverseIndex(6, index) * 0.1,
                 }}
