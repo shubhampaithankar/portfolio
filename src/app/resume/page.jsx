b1e3f90a7c312946be81552187219f6c01ee5fcd
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-07-09 16:44:20 +0530

    basic fethcing in resume page

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,5 +1,34 @@
-import React from 'react'
+'use client'
+
+import { useEffect, useState } from 'react'
 
 export default function Resume() {
+    const [resumeData, setResumeData] = useState('')
+
+    const fetchResume = async () => {
+        try {
+            const URL = 'https://shubhampaithankar.github.io/resume/resume.pdf'
+            fetch(URL)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok')
+                    }
+                    return response.blob()
+                })
+                .then((file) => {})
+            // setResumeData(data)
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error)
+            return
+        }
+    }
+
+    useEffect(() => {
+        fetchResume()
+    }, [])
+
+    useEffect(() => {
+        console.log(resumeData)
+    }, [resumeData])
     return <div>WIP</div>
 }
