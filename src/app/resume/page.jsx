764a94d2b3b3c7e3883e6964794ccca4a31530a4
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-07-09 22:21:47 +0530

    added a way to parse resume: test edenai

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -9,14 +9,37 @@ export default function Resume() {
         try {
             const URL = 'https://shubhampaithankar.github.io/resume/resume.pdf'
             fetch(URL)
-                .then((response) => {
+                .then(async (response) => {
                     if (!response.ok) {
                         throw new Error('Network response was not ok')
                     }
-                    return response.blob()
+                    const blob = await response.blob()
+                    const file = new File([blob], 'resume.pdf', {
+                        lastModified: new Date().getTime(),
+                        type: blob.type,
+                    })
+                    return file
                 })
-                .then((file) => {})
-            // setResumeData(data)
+                .then(async (file) => {
+                    const formData = new FormData()
+                    formData.append('providers', 'affinda')
+                    formData.append('file', file)
+
+                    try {
+                        const response = await fetch(
+                            'https://api.edenai.run/v2/ocr/resume_parser',
+                            {
+                                method: 'POST',
+                                headers: {
+                                    Authorization: 'Bearer api-key',
+                                },
+                                body: formData,
+                            },
+                        )
+                        const data = await response.json()
+                        setResumeData(data)
+                    } catch (error) {}
+                })
         } catch (error) {
             console.error('There was a problem with the fetch operation:', error)
             return
@@ -30,5 +53,6 @@ export default function Resume() {
     useEffect(() => {
         console.log(resumeData)
     }, [resumeData])
+
     return <div>WIP</div>
 }
