7930125038dd1a64db55b6e3ac81772761718229
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-07-10 16:52:33 +0530

    added better api to fetch and read resume

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,58 +1,62 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import { motion } from 'framer-motion'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export default function Resume() {
-    const [resumeData, setResumeData] = useState('')
-
-    const fetchResume = async () => {
-        try {
-            const URL = 'https://shubhampaithankar.github.io/resume/resume.pdf'
-            fetch(URL)
-                .then(async (response) => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok')
-                    }
-                    const blob = await response.blob()
-                    const file = new File([blob], 'resume.pdf', {
-                        lastModified: new Date().getTime(),
-                        type: blob.type,
-                    })
-                    return file
-                })
-                .then(async (file) => {
-                    const formData = new FormData()
-                    formData.append('providers', 'affinda')
-                    formData.append('file', file)
-
-                    try {
-                        const response = await fetch(
-                            'https://api.edenai.run/v2/ocr/resume_parser',
-                            {
-                                method: 'POST',
-                                headers: {
-                                    Authorization: 'Bearer api-key',
-                                },
-                                body: formData,
-                            },
-                        )
-                        const data = await response.json()
-                        setResumeData(data)
-                    } catch (error) {}
-                })
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error)
-            return
-        }
+    const [resume, setResume] = useState()
+    const fetchResumeAsJSON = async () => {
+        const RESUME_URL = 'https://shubhampaithankar.github.io/resume/resume.pdf'
+        const API_URL = 'https://worke.formextractorai.com/v2/extract'
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                accept: 'application/json:',
+                'X-WORKER-EXTRACTOR-ID': '',
+                'X-WORKER-IMAGE-URL': RESUME_URL,
+                'X-WORKER-ENCODING': 'raw',
+                'X-WORKER-PDF-DPI': '150',
+                'X-WORKER-ASYNC': 'false',
+                'X-WORKER-AUTO-ADJUST-IMAGE-SIZE': 'true',
+                'X-WORKER-PROCESSING-MODE': 'per-page',
+                'content-type': 'image/*',
+                'X-WORKER-TOKEN': '',
+            },
+            cache: 'force-cache',
+        })
+        if (!response.ok) throw new Error(`API request failed with status ${response.status}`)
+        const data = await response.json()
+        setResume(data.documents[0].data)
     }
 
-    useEffect(() => {
-        fetchResume()
-    }, [])
-
-    useEffect(() => {
-        console.log(resumeData)
-    }, [resumeData])
-
-    return <div>WIP</div>
+    useEffect(() => fetchResumeAsJSON(), [])
+    return (
+        <motion.div
+            initial={{
+                opacity: 0,
+            }}
+            animate={{
+                opacity: 1,
+                transition: {
+                    delay: 2.4,
+                    duration: 0.4,
+                    ease: 'easeIn',
+                },
+            }}
+            className="flex min-h-[80vh] items-center justify-center py-12 xl:py-0"
+        >
+            <div className="container mx-auto">
+                <Tabs defaultValue="experience" className="flex flex-col xl:flex-row">
+                    <TabsList>
+                        {resume &&
+                            Object.keys(resume).map((field, index) => {
+                                return <TabsTrigger key={index}>{field}</TabsTrigger>
+                            })}
+                    </TabsList>
+                </Tabs>
+            </div>
+        </motion.div>
+    )
 }
